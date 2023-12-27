0e5c4de163657fe39421a419a431e5b68c510481
HQTrong <huynhquoctrong2703@gmail.com>
2023-12-27 22:52:47 +0700

    Update Spring MVC

--- src/main/java/com/example/Service/EmployeeService.java
+++ src/main/java/com/example/Service/EmployeeService.java
@@ -1,9 +1,10 @@
 package com.example.Service;
 
 import com.example.Entity.Employee;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public interface EmployeeService {
 List<Employee> listEm ();
 }
